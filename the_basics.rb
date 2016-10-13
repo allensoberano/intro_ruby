4293d4a88d7195f96690d4433244d62c0833d8ef
Allen Soberano <allensoberano@gmail.com>
2016-10-12 22:48:14 -0700

    Finished The Basics

---
+++ the_basics.rb
@@ -0,0 +1,51 @@
+puts "EXCERCISE 1-----------------"
+puts "Allen" + " " + "Soberano"
+puts "Allen " + "Soberano"
+puts "Allen" + " Soberano"
+first_name = "Allen"
+last_name = "Soberano"
+puts first_name + " " + last_name
+puts "#{first_name} #{last_name}"
+
+puts "EXCERCISE 2-----------------"
+number_to_digit = 1357
+puts "#{number_to_digit/1000} Thousands"
+puts "#{number_to_digit%1000/100} Hundreds"
+puts "#{number_to_digit%1000%100/10} Tens"
+puts "#{number_to_digit%1000%100%10} Ones"
+
+puts "EXCERCISE 3 ----------------"
+movie_hash = {
+                :gone_with_the_wind => 1939,
+                :casablanca => 1942,
+                :the_wizard_of_ox => 1939,
+                :to_kill_a_mockingbird => 1962
+              }
+puts movie_hash.values
+
+puts "EXCERCISE 4 ----------------"
+movie_date_array = [1939, 1942, 1939, 1962]
+puts movie_date_array[0]
+puts movie_date_array[1]
+puts movie_date_array[2]
+puts movie_date_array[3]
+
+
+puts "EXCERCISE 5 ----------------"
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+
+puts "EXCERCISE 6 ----------------"
+puts 5.5 * 5.5
+puts 6.6 * 6.6
+puts 7.7 * 7.7
+
+puts "EXCERCISE 7 ----------------"
+puts "Developer put a parentheses instead of curly bracket. Usually when creating a hash"
+
+
+
+
